a3f78fdce74cabcb4b9fc60c96155d57f8f652b2
xAzerty1 <104255561+xAzerty1@users.noreply.github.com>
2024-07-12 15:00:16 +0200

    Add files via upload

---
+++ JS.js
@@ -0,0 +1,65 @@
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('emailForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        var email = document.getElementById('emailInput').value;
+
+        var emails = generateEmailsWithDotTrick(email);
+
+        var emailListDiv = document.getElementById('emailList');
+        emailListDiv.innerHTML = '';
+
+        var heading = document.createElement('h3');
+        heading.textContent = 'Sucessfully Generated the following Emails :';
+        emailListDiv.appendChild(heading);
+
+        var ol = document.createElement('ol');
+        ol.style.listStyleType = 'disc';
+        emails.forEach(function(email) {
+            var li = document.createElement('li');
+            li.textContent = email;
+            ol.appendChild(li);
+        });
+        emailListDiv.appendChild(ol);
+    });
+
+    function generateEmailsWithDotTrick(email) {
+        var parts = email.split('@');
+        var username = parts[0];
+        var domain = parts[1];
+
+        var emails = [];
+
+        generateVariants(username, '', 0, emails, domain);
+
+        return emails;
+    }
+
+    function generateVariants(username, currentEmail, index, emails, domain) {
+        if (index === username.length) {
+            emails.push(currentEmail + '@' + domain);
+            return;
+        }
+
+        generateVariants(username, currentEmail + username[index], index + 1, emails, domain);
+
+        generateVariants(username, currentEmail + username[index] + '.', index + 1, emails, domain);
+    }
+
+
+    var frame = document.getElementById("simple-cookie-consent");
+    if (frame) {
+        var denyButton = frame.querySelector(".cookie-consent-deny");
+        var allowButton = frame.querySelector(".cookie-consent-allow");
+
+        denyButton.addEventListener("click", function() {
+            frame.remove();
+        });
+
+        allowButton.addEventListener("click", function() {
+            frame.remove();
+        });
+    } else {
+        console.error("Frame element not found.");
+    }
+});
